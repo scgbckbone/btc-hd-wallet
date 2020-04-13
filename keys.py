ba7f842099b000ed8def469d39b90851bf6caaaa
scgbckbone <virgovica@gmail.com>
2020-04-13 15:44:28 +0200

    added type hints for 'keys.py' module

--- keys.py
+++ keys.py
@@ -1,4 +1,5 @@
 import ecdsa
+from typing import Union
 
 from helper import (
     encode_base58_checksum, decode_base58_checksum, big_endian_to_int,
@@ -7,6 +8,7 @@ from helper import (
 
 
 SECP256k1 = ecdsa.curves.SECP256k1
+any_point = Union[ecdsa.ellipticcurve.Point, ecdsa.ellipticcurve.PointJacobi]
 
 
 class PrivateKey(object):
@@ -28,16 +30,16 @@ class PrivateKey(object):
     def __bytes__(self) -> bytes:
         return self.k.to_string()
 
-    def __eq__(self, other) -> bool:
+    def __eq__(self, other: "PrivateKey") -> bool:
         return self.sec_exp == other.sec_exp
 
-    def wif(self, compressed=True, testnet=False) -> str:
+    def wif(self, compressed: bool = True, testnet: bool = False) -> str:
         prefix = b"\xef" if testnet else b"\x80"
         suffix = b"\x01" if compressed else b""
         return encode_base58_checksum(prefix + bytes(self) + suffix)
 
     @classmethod
-    def from_wif(cls, wif_str: str):
+    def from_wif(cls, wif_str: str) -> "PrivateKey":
         decoded = decode_base58_checksum(s=wif_str)
         if wif_str[0] in ("K", "L", "c"):
             # compressed key --> so remove last byte that has to be 01
@@ -46,7 +48,7 @@ class PrivateKey(object):
         return cls(sec_exp=big_endian_to_int(decoded[1:]))
 
     @classmethod
-    def parse(cls, byte_str: bytes):
+    def parse(cls, byte_str: bytes) -> "PrivateKey":
         return cls(sec_exp=big_endian_to_int(byte_str))
 
 
@@ -60,26 +62,27 @@ class PublicKey(object):
         self.K = key
 
     @property
-    def point(self):
+    def point(self) -> ecdsa.ellipticcurve.Point:
         return self.K.pubkey.point
 
-    def sec(self, compressed=True) -> bytes:
+    def sec(self, compressed: bool = True) -> bytes:
         if compressed:
             return self.K.to_string(encoding="compressed")
         return self.K.to_string(encoding="uncompressed")
 
     @classmethod
-    def parse(cls, key_bytes):
+    def parse(cls, key_bytes: bytes) -> "PublicKey":
         return cls(ecdsa.VerifyingKey.from_string(key_bytes, curve=SECP256k1))
 
     @classmethod
-    def from_point(cls, point):
+    def from_point(cls, point: any_point) -> "PublicKey":
         return cls(ecdsa.VerifyingKey.from_public_point(point, curve=SECP256k1))
 
-    def h160(self, compressed=True) -> bytes:
+    def h160(self, compressed: bool = True) -> bytes:
         return hash160(self.sec(compressed=compressed))
 
-    def address(self, compressed=True, testnet=False, addr_type="p2pkh") -> str:
+    def address(self, compressed: bool = True, testnet: bool = False,
+                addr_type: str = "p2pkh") -> str:
         h160 = self.h160(compressed=compressed)
         if addr_type == "p2pkh":
             return h160_to_p2pkh_address(h160=h160, testnet=testnet)
