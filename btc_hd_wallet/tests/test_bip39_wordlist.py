5ceb661c318e314bc4170e43682b4ecd965394c6
scgbckbone <virgovica@gmail.com>
2020-04-23 17:51:39 +0200

    wordlist - is not removed during the tests, only renamed and backed up in case no internet connection is available after download

--- btc_hd_wallet/tests/test_bip39_wordlist.py
+++ btc_hd_wallet/tests/test_bip39_wordlist.py
@@ -7,12 +7,21 @@ from btc_hd_wallet.bip39_wordlist import get_word_list
 class TestBip39WordList(unittest.TestCase):
 
     word_list_path = "btc_hd_wallet/bip39_wordlist/english.txt"
+    word_list_bak_path = word_list_path + ".bak"
 
-    def remove_word_list_file(self):
-        os.remove(self.word_list_path)
+    def setUp(self) -> None:
+        if os.path.exists(self.word_list_bak_path):
+            os.remove(self.word_list_bak_path)
+        if os.path.exists(self.word_list_path):
+            os.rename(self.word_list_path, self.word_list_bak_path)
+
+    def tearDown(self) -> None:
+        if os.path.exists(self.word_list_path):
+            os.remove(self.word_list_bak_path)
+        else:
+            os.rename(self.word_list_bak_path, self.word_list_path)
 
     def test_download(self):
-        self.remove_word_list_file()
         word_list = get_word_list()
         self.assertIsInstance(word_list, list)
         self.assertEqual(len(word_list), 2048)
