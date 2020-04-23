564c6560f79b430a52303c4d88da97b10908865e
scgbckbone <virgovica@gmail.com>
2020-04-23 10:44:29 +0200

    updated classifiers to state that only Python3 is supported

--- setup.py
+++ setup.py
@@ -23,7 +23,12 @@ setup(
     long_description_content_type='text/markdown',
     classifiers=[
       "Development Status :: 3 - Alpha"
+      "Programming Language :: Python :: 3 :: Only",
+      "Programming Language :: Python :: 3.5",
       "Programming Language :: Python :: 3.6",
+      "Programming Language :: Python :: 3.7",
+      "Programming Language :: Python :: 3.8",
+      "Programming Language :: Python :: 3.9",
     ],
     url='https://github.com/scgbckbone/btc-hd-wallet',
     keywords='bitcoin',
