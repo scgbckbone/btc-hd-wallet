24f2ecafdae897b2a0da84c45d300c0364297cd5
scgbckbone <virgovica@gmail.com>
2020-04-26 10:11:01 +0200

    added more names to btc_hd_wallet scope from child scopes

--- btc_hd_wallet/__init__.py
+++ btc_hd_wallet/__init__.py
@@ -1,2 +1,8 @@
 from btc_hd_wallet.paper_wallet import PaperWallet
 from btc_hd_wallet.base_wallet import BaseWallet
+from btc_hd_wallet.bip39 import (
+    bip39_seed_from_mnemonic, mnemonic_from_entropy_bits, mnemonic_from_entropy
+)
+from btc_hd_wallet.bip32 import PrvKeyNode, PubKeyNode
+from btc_hd_wallet.keys import PublicKey, PrivateKey
+from btc_hd_wallet.script import Script
